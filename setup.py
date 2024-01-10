98a6c5dfece5ab9ffe4abe4cd5433993dfdea3e5
animebing <yanbing_dong@hotmail.com>
2024-01-11 00:33:57 +0800

    update

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-keyup-react",
-    version="0.0.1",
+    version="0.1.0",
     author="Yanbing Dong",
     author_email="yanbing_dong@hotmail.com",
     description="Text input that renders on keyup",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/animebing/streamlit-keyup-react",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
